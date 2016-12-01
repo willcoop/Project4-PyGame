25f826bb4d27d009fc7fff3cb539406082d12f8f
Will Cooper <willcooper@Wills-MacBook-Pro.local>
2016-11-30 23:13:42 -0500

    updated key definition in snake

--- wc_pygame.py
+++ wc_pygame.py
@@ -8,6 +8,8 @@ class main:
 
     def __init__(self, width=500, height=480):
         pygame.init() #initialize Pygame
+        pygame.display.set_caption('Coops Game')
+
 
         #setting window size
         self.width = width
@@ -15,21 +17,41 @@ class main:
 
         #creating screen
         self.screen = pygame.display.set_mode((self.width, self.height))
+        
 
-    def MainLoop(self):
-    	"""Main loop of game"""
-    	while 1:
-    		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-    				sys.exit()
+class Snake:
+#create Snake
+    def __init__(self, surface):
+        self.surface = surface
+        self.x = surface.get_width() / 2
+        self.y = surface.get_width() / 2
+        self.length = pieceSize
+        self.grow_to = pieceSize*2
+        self.xVel = 0
+        self.yVel = -pieceSize
+        self.body = []
+        self.head = None
+        self.crashed = False
+        self.color = WHITE
+
+    def keys(self):
+
+        key = pygame.key.get_pressed()
+        dist = 1
+        if key[pygame.K_DOWN]:
+            self.y += dist
+        if key[pygame.K_UP]:
+            self.y -= dist
+        if key[pygame.K_RIGHT]:
+            self.x += dist
+        if key[pygame.K_LEFT]:
+            self.y -= dist
+
+    def draw(self, surface):
+        surface.blit(self.image,(self.x,self.y))
 
 
-    
 
-class Snake(pygame.sprite.Sprite):
-    "This is our snake that will move around the screen"
 
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_image('ship.png')
-        self.pellets = 0
+
+
